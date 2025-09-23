e2609b603821c4c8aa7c142e81e435ead08d4348
DaniilGordeevv <daniilgor1234@gmail.com>
2025-09-23 20:10:41 +0300

    отредактирован modAlphaCipher.h

--- lab1_1/modAlphaCipher.h
+++ lab1_1/modAlphaCipher.h
@@ -1,22 +1,20 @@
 #pragma once
-#include <map>
-#include <string>
 #include <vector>
-#include <locale>
-#include <codecvt>
+#include <string>
+#include <map>
 
-class modAlphaCipher
-{
-private:
-    std::wstring numAlpha = L"АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"; // русский алфавит
-    std::map<wchar_t, int> alphaNum; // ассоциативный массив "номер по символу"
-    std::vector<int> key;         // ключ
-    std::vector<int> convert(const std::wstring& s); // преобразование строка-вектор
-    std::wstring convert(const std::vector<int>& v); // преобразование вектор-строка
-    
-public:
-    modAlphaCipher() = delete;               // запретим конструктор без параметров
-    modAlphaCipher(const std::wstring& skey); // конструктор для установки ключа
-    std::wstring encrypt(const std::wstring& open_text);   // зашифрование
-    std::wstring decrypt(const std::wstring& cipher_text); // расшифрование
+using namespace std;
+
+class modAlphaCipher {
+    private:
+        wstring numAlpha = L"АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+        map <wchar_t,int> alphaNum;
+        vector <int> key;
+        vector<int> convert(const wstring& s);
+        wstring convert(const vector<int>& v);
+    public:
+        modAlphaCipher()=delete;
+        modAlphaCipher(const wstring& skey);
+        wstring encrypt(const wstring& open_text);
+        wstring decrypt(const wstring& cipher_text);
 };
